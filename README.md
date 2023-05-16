0f20775a950622e7fe92759f37d24337ac2a5362
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-05-16 16:04:50 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A website for an online furniture store.
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '48rem',
+      md: '64rem',
+      lg: '85.375rem',
+      xl: '120rem',
+      '2xl': '160rem'
+    },
+    extend: {
+      colors: {
+        primary: {
+          300: '#E5E2D8',
+          700: '#999381'
+        },
+        accents: {
+          300: '#D8DAE6',
+          700: '#717799'
+        },
+        text: '#212427'
+      }
+    },
+  },
+  plugins: [],
+}
+
