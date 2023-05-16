639f4bf2cf405fba22c9f3ed012b4dc0278d0478
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-05-16 16:43:03 +0200

    Add react-icons dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Header } from './containers';
 import './App.css';
 
 function App() {
-  return <div className="mx-auto" />;
+  return (
+    <div className="mx-auto 2xl:mx-[18%]">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
