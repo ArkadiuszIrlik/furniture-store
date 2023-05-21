78a3c5440b8ab696077527183870deea8c07ada9
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-05-21 17:39:18 +0200

    Update site-wide text color

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Header } from './containers';
+import Search from './components/Search';
+import { RecommendedCarousel } from './components';
 import './App.css';
 
 function App() {
   return (
-    <div className="mx-auto 2xl:mx-[18%]">
+    <div className="mx-auto 2xl:mx-[18%] text-text">
       <Header />
+      <RecommendedCarousel />
+      {/* <Search /> */}
     </div>
   );
 }
