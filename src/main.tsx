340c6045fb5d6e59290dbb1f54011b931c4afdce
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-05-21 17:36:49 +0200

    Add swiper dependency

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
