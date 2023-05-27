55962795595b3262387d9cf4333522ce2c8ec812
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-05-27 15:07:24 +0200

    Make accessibility changes to components

--- src/containers/header/Header.tsx
+++ src/containers/header/Header.tsx
@@ -15,52 +15,54 @@ function Header() {
   const mediumMatches = useMediaQuery(`(min-width: ${mediumScreen})`);
 
   return (
-    <div
-      className="grid grid-cols-[1fr_minmax(max-content,_60%)_1fr]
-         grid-rows-[repeat(2,_max-content)] md:mx-[10%] items-center
-          py-4 px-3 gap-y-4"
-    >
-      <IconContext.Provider
-        value={{
-          style: { strokeWidth: '0.05rem', stroke: textColor },
-          size: '1.5rem',
-        }}
+    <header>
+      <div
+        className="grid grid-cols-[1fr_minmax(max-content,_60%)_1fr]
+           grid-rows-[repeat(2,_max-content)] md:mx-[10%] items-center
+            py-4 px-3 gap-y-4"
       >
-        {!mediumMatches && <HiMenu />}
-      </IconContext.Provider>
-      <img
-        src={ElevaLogo}
-        alt="Eleva Design Logo"
-        srcSet=""
-        className="justify-self-center md:justify-self-start w-28"
-      />
-      <div className="col-span-full row-start-2 md:col-span-1 md:col-start-2 md:row-start-1">
-        <Search />
-      </div>
-      <div className="flex gap-2 justify-end items-center">
         <IconContext.Provider
           value={{
             style: { strokeWidth: '0.05rem', stroke: textColor },
             size: '1.5rem',
           }}
         >
-          <CiUser />
+          {!mediumMatches && <HiMenu />}
         </IconContext.Provider>
-        <IconContext.Provider
-          value={{
-            style: { strokeWidth: '0.1rem', stroke: textColor },
-            size: '1.5rem',
-          }}
-        >
-          <FiShoppingCart />
-        </IconContext.Provider>
-      </div>
-      {mediumMatches && (
-        <div className="col-span-full row-start-2 ">
-          <HeaderNavbar />
+        <img
+          src={ElevaLogo}
+          alt="Eleva Design Logo"
+          srcSet=""
+          className="justify-self-center md:justify-self-start w-28"
+        />
+        <div className="col-span-full row-start-2 md:col-span-1 md:col-start-2 md:row-start-1">
+          <Search />
         </div>
-      )}
-    </div>
+        <div className="flex gap-2 justify-end items-center">
+          <IconContext.Provider
+            value={{
+              style: { strokeWidth: '0.05rem', stroke: textColor },
+              size: '1.5rem',
+            }}
+          >
+            <CiUser />
+          </IconContext.Provider>
+          <IconContext.Provider
+            value={{
+              style: { strokeWidth: '0.1rem', stroke: textColor },
+              size: '1.5rem',
+            }}
+          >
+            <FiShoppingCart />
+          </IconContext.Provider>
+        </div>
+        {mediumMatches && (
+          <div className="col-span-full row-start-2 ">
+            <HeaderNavbar />
+          </div>
+        )}
+      </div>
+    </header>
   );
 }
 export default Header;
