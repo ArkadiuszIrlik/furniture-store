dd643733a04be29a2854235e7b0a14f800d54217
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-06-04 21:22:09 +0200

    Display color name on swatch hover

--- src/components/ProductColorSelector.tsx
+++ src/components/ProductColorSelector.tsx
@@ -19,35 +19,41 @@ function ProductColorSelector({
           </legend>
           {colorList.map((color, index) => {
             return (
-              <label
-                className={`rounded-lg border-2 overflow-hidden cursor-pointer
-                    ${
-                      index === selectedColorIndex
-                        ? 'border-primary-700'
-                        : 'border-white'
-                    } `}
-                htmlFor={`input-color-${color.name}`}
-                aria-label={color.name}
-              >
-                <input
-                  type="radio"
-                  name="product-color-selection"
-                  id={`input-color-${color.name}`}
-                  value={index}
-                  checked={index === selectedColorIndex}
-                  className="sr-only"
-                  onChange={() => onChangeSelection(index)}
-                />
-                <img
-                  src={color.image}
-                  alt=""
-                  srcSet=""
-                  className={`${
-                    index === selectedColorIndex ? 'p-1' : ''
-                  } w-12 aspect-square rounded-lg`}
-                  draggable="false"
-                />
-              </label>
+              <div className="flex relative">
+                <label
+                  className={`rounded-lg border-2 overflow-hidden cursor-pointer
+                      ${
+                        index === selectedColorIndex
+                          ? 'border-primary-700'
+                          : 'border-white'
+                      } `}
+                  htmlFor={`input-color-${color.name}`}
+                  aria-label={color.name}
+                >
+                  <input
+                    type="radio"
+                    name="product-color-selection"
+                    id={`input-color-${color.name}`}
+                    value={index}
+                    checked={index === selectedColorIndex}
+                    className="sr-only"
+                    onChange={() => onChangeSelection(index)}
+                  />
+                  <img
+                    src={color.image}
+                    alt=""
+                    srcSet=""
+                    className={`peer ${
+                      index === selectedColorIndex ? 'p-1' : ''
+                    } w-12 aspect-square rounded-lg`}
+                    draggable="false"
+                  />
+                  <p className="hidden peer-hover:block absolute top-[110%] left-1/2
+                    bg-primary-300 text-sm w-max px-1 -translate-x-1/2 font-dm-sans"
+                    aria-hidden="true"
+                    >{color.name}</p>
+                </label>
+              </div>
             );
           })}
         </fieldset>
