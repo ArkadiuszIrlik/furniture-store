3c04a8629b41d666ce009539cef96f9d2562a47a
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-06-04 20:43:08 +0200

    Change page container width breakpoints
    I noticed that the page was display far too wide on high resolution
    displays. These changes help fix that issue to some extent.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="mx-auto 2xl:mx-[18%] min-h-screen text-text flex flex-col">
+    <div className="mx-auto lg:mx-[8%] xl:mx-[14%] 2xl:mx-auto 2xl:max-w-[1800px] min-h-screen text-text flex flex-col">
       <Header />
       {/* <Category /> */}
       <Product />
