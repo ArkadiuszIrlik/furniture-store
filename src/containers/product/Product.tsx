ad64637f76a9683d5b6ae5a39e901dc37659eea5
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-06-05 22:24:51 +0200

    Add info section to Product container

--- src/containers/product/Product.tsx
+++ src/containers/product/Product.tsx
@@ -1,5 +1,8 @@
 import resolveConfig from 'tailwindcss/resolveConfig';
 import { useState } from 'react';
+import { BsTruck } from 'react-icons/bs';
+import { BiPlus, BiMinus } from 'react-icons/bi';
+import { IconContext } from 'react-icons';
 import {
   PathDisplay,
   ProductColorSelector,
@@ -37,6 +40,8 @@ function Product() {
     name: 'Adara Bed',
     priceUsd: 1249,
     images: [bed1, bed2, bed3, bed4, bed5, bed6, bed7, bed8, bed9],
+    description:
+      'A stately bed frame made to fit a serene space. Its subtle winged headboard and trim lines offer crisp sophistication. Bring a bespoke touch to your bedroom with this upholstered bed that suits and elevates a tailored environment.',
     reviewScore: 3.76,
     reviews: [
       {
@@ -58,12 +63,56 @@ function Product() {
       { name: 'Prep School Plaid', image: prepSchoolPlaid },
     ],
     sizes: [{ name: 'Cal King' }, { name: 'King' }, { name: 'Queen' }],
+    information: [
+      {
+        header: 'Details',
+        content: [
+          'made to order items are not returnable',
+          'Ships in: 2-4 Weeks',
+          'material: Pine wood, polyurethane',
+          'fabric content: 95% Polyester, 5% Linen',
+          'fabric color: Talc Linen',
+          'Box spring required',
+          'center support leg: Yes',
+          'assembly required: Yes',
+          'Made In: USA with imported materials',
+        ],
+      },
+      {
+        header: 'Dimensions',
+        content: [
+          'Overall Dimensions: 77"W x 89"D x 55"H',
+          'product weight: 140 lbs',
+          'Footboard Width: 72" ',
+          'Distance from Bottom of Headboard to Mattress Platform: 18"H',
+          'mattress cavity: 7.5"',
+          'headboard dimensions: 77"W x 11"D x 55"H',
+          'side rail height: 13.5"',
+          'height to top of side rail: 13.5"',
+          'under bed clearance: 4"',
+        ],
+      },
+      {
+        header: 'Care',
+        content: [
+          'Avoid placing upholstery in direct sunlight to minimize fading.',
+          'Depending on the fabric, pilling or crushing may occur with wear.',
+          'Spot clean by blotting with a light-colored, damp …end contacting a professional upholstery cleaner.',
+          'Vacuum regularly with an upholstery attachment, and fluff and flip cushions as needed.',
+        ],
+      },
+    ],
   };
 
   const [activeImage, setActiveImage] = useState(0);
   // Only set this if the color property exists on the product
   const [selectedColorIndex, setSelectedColorIndex] = useState(0);
   const [selectedSizeIndex, setSelectedSizeIndex] = useState(0);
+  const [infoDropdownOpenState, setInfoDropdownOpenState] = useState(
+    product.information.map((section) => {
+      return { isOpen: false };
+    })
+  );
 
   // const reviewPercentage = Math.round(product.score / 0.5) * 0.5;
   const reviewScorePercentage = `${Math.floor(product.reviewScore * 20)}%`;
@@ -76,6 +125,16 @@ function Product() {
   }
   function handleSwitchSelectedSizeIndex(sizeIndex: number) {
     setSelectedSizeIndex(sizeIndex);
+  }
+  function handleToggleInfoSectionOpen(sectionIndex: number) {
+    setInfoDropdownOpenState(
+      infoDropdownOpenState.map((infoSection, index) => {
+        if (index === sectionIndex) {
+          return { ...infoSection, isOpen: !infoSection.isOpen };
+        }
+        return infoSection;
+      })
+    );
   }
 
   const pathToProduct = [
@@ -151,7 +210,10 @@ function Product() {
                 .toString()
                 .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
             </p>
-            <div className="border-y-[1px] border-primary-300 py-4">
+            <div
+              className="border-y-[1px] border-primary-300 py-4 flex
+              flex-col gap-3"
+            >
               <ProductColorSelector
                 colorList={product.colors}
                 selectedColorIndex={selectedColorIndex}
@@ -172,7 +234,82 @@ function Product() {
                   <PrimaryButton>ADD TO CART</PrimaryButton>
                 </div>
               </div>
+              <div className="flex items-center gap-2">
+                <IconContext.Provider
+                  value={{
+                    style: { strokeWidth: '0.05rem' },
+                    className: 'text-text',
+                    size: '1.4rem',
+                  }}
+                >
+                  <BsTruck />
+                </IconContext.Provider>
+                <p className="font-open-sans">
+                  In stock! Ships in 5–10 business days
+                </p>
+              </div>
             </div>
+            <div className="py-4">
+              <p className="font-open-sans">{product.description}</p>
+            </div>
+            {product.information.map((info, index) => {
+              return (
+                <div key={index}>
+                  <div
+                    className="border-t-[1px] border-t-primary-300 py-2 flex
+                   justify-between items-center pr-1
+                   "
+                  >
+                    <h4 className="font-dm-sans text-xl">
+                      {info.header.toUpperCase()}
+                    </h4>
+                    <button
+                      type="button"
+                      aria-label={
+                        infoDropdownOpenState[index].isOpen
+                          ? 'close section'
+                          : 'open section'
+                      }
+                      onClick={() => handleToggleInfoSectionOpen(index)}
+                    >
+                      <IconContext.Provider
+                        value={{
+                          style: {
+                            strokeWidth: '0.03rem',
+                          },
+                          size: '1.7rem',
+                          className: 'text-primary-700',
+                        }}
+                      >
+                        {infoDropdownOpenState[index].isOpen ? (
+                          <BiMinus />
+                        ) : (
+                          <BiPlus />
+                        )}
+                      </IconContext.Provider>
+                    </button>
+                  </div>
+                  {infoDropdownOpenState[index].isOpen && (
+                    <div className="border-t-[1px] border-t-primary-300 py-3">
+                      <ul className="flex flex-col gap-[2px]">
+                        {info.content.map((bulletpoint) => {
+                          return (
+                            <li
+                              className='relative font-open-sans before:content-["·"]
+                          before:align-middle before:text-3xl before:leading-6 before:font-open-sans
+                          before:absolute before:left-[-0.5em] ml-4'
+                            >
+                              {bulletpoint[0].toUpperCase() +
+                                bulletpoint.slice(1)}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
