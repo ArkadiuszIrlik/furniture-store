8206165ea095fa0f2212244dfe4221f871e6528f
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-06-08 14:37:31 +0200

    Clean up SpinButton component

--- src/components/SpinButton.tsx
+++ src/components/SpinButton.tsx
@@ -25,13 +25,12 @@ function SpinButton({
       <label htmlFor={inputId} className="block font-dm-sans mb-1">
         {labelText}
       </label>
-      {/* <div className="rounded-md bg-gray-400 grid grid-cols-[2rem,_4rem,_2rem]"> */}
-      <div className="rounded-md border-2 border-neutral-200 bg-neutral-200 grid grid-cols-3">
+      <div
+        className="rounded-md border-2 border-neutral-200 bg-neutral-200
+         grid grid-cols-3"
+      >
         <button
           type="button"
-          // className="border-r-[2px] border-r-white"
-          // className="border-r-[1px] border-r-neutral-300 flex
-          // justify-center items-center"
           className=" flex
           justify-center items-center"
           tabIndex={-1}
@@ -61,21 +60,12 @@ function SpinButton({
             border-none font-dm-sans px-0 py-1
             bg-transparent w-full text-center
               focus:ring-0 focus:border-white
-              focus:outline-black focus:-outline-offset-2
-
-
-               "
-            //  border-x-[1px] border-x-white border-y-0
-            //  focus:border-red-400 focus:border-2 focus:border-solid
-            //  focus:outline-black focus:outline-offset-0
+              focus:outline-black focus:-outline-offset-2"
           />
         </div>
         <button
           type="button"
-          // className="border-l-[2px] border-l-white"
-          // className="border-l-[1px] border-l-neutral-300 flex
-          // justify-center items-center"
-          className="flex
+          className="flex 
           justify-center items-center"
           tabIndex={-1}
           aria-hidden="true"
