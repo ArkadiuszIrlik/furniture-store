f78e0fdda990020e5a96c0bd29a53d7cdc2d142c
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-06-08 13:41:35 +0200

    Fix CartQuantityCounter clipped on mobile

--- src/components/CartQuantityCounter.tsx
+++ src/components/CartQuantityCounter.tsx
@@ -28,7 +28,7 @@ function CartQuantityCounter() {
       {!!cartQuantity && (
         <div
           className="absolute bg-accents-700 rounded-full aspect-square min-w-[1.5rem] min-h-0 
-             p-1 right-0 bottom-0 translate-x-[60%] translate-y-[60%] flex
+             p-1 right-0 bottom-0 translate-x-[40%] translate-y-[80%] md:translate-x-[60%] md:translate-y-[60%] flex
              justify-center items-center"
         >
           <p className="font-dm-sans font-medium text-base text-white">
