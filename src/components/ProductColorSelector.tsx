586c755946f93ad9b9e6fa001dcc06bde3677530
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-06-08 14:35:05 +0200

    Add hover styles to ProductColorSelector

--- src/components/ProductColorSelector.tsx
+++ src/components/ProductColorSelector.tsx
@@ -21,7 +21,8 @@ function ProductColorSelector({
             return (
               <div className="flex relative">
                 <label
-                  className={`rounded-lg border-2 overflow-hidden cursor-pointer
+                  className={`rounded-lg border-2 overflow-hidden 
+                  cursor-pointer hover:border-primary-700
                       ${
                         index === selectedColorIndex
                           ? 'border-primary-700'
@@ -45,13 +46,16 @@ function ProductColorSelector({
                     srcSet=""
                     className={`peer ${
                       index === selectedColorIndex ? 'p-1' : ''
-                    } w-12 aspect-square rounded-lg`}
+                    } hover:p-1 w-12 aspect-square rounded-lg`}
                     draggable="false"
                   />
-                  <p className="hidden peer-hover:block absolute top-[110%] left-1/2
+                  <p
+                    className="hidden peer-hover:block absolute top-[110%] left-1/2
                     bg-primary-300 text-sm w-max px-1 -translate-x-1/2 font-dm-sans"
                     aria-hidden="true"
-                    >{color.name}</p>
+                  >
+                    {color.name}
+                  </p>
                 </label>
               </div>
             );
