6969716e5b8804a7fad99a79b5945574f6d50728
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-06-09 00:07:53 +0200

    Add addToCart function

--- src/containers/product/Product.tsx
+++ src/containers/product/Product.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { BsTruck } from 'react-icons/bs';
 import { BiPlus, BiMinus } from 'react-icons/bi';
 import { IconContext } from 'react-icons';
+import { v4 as uuidv4 } from 'uuid';
 import {
   PathDisplay,
   ProductColorSelector,
@@ -39,7 +40,8 @@ function Product() {
   const colorAccents700 = fullConfig.theme.colors.accents['700'];
   const colorAccents300 = fullConfig.theme.colors.accents['300'];
 
-  const product = {
+  const [product, setProduct] = useState({
+    id: uuidv4(),
     name: 'Adara Bed',
     priceUsd: 1249,
     images: [bed1, bed2, bed3, bed4, bed5, bed6, bed7, bed8, bed9],
@@ -121,7 +123,7 @@ function Product() {
         ],
       },
     ],
-  };
+  });
 
   const [activeImage, setActiveImage] = useState(0);
   // Only set this if the color property exists on the product
@@ -166,6 +168,45 @@ function Product() {
     { name: 'Beds' },
     { name: 'Adara Bed' },
   ];
+
+  function addToCart(productId: string, quantity: number): void {
+    let cart = {};
+    if (localStorage.getItem('cart')) {
+      cart = JSON.parse(localStorage.getItem('cart'));
+    }
+    if (productId in cart) {
+      cart[productId].quantity += quantity;
+    } else {
+      cart[productId] = { quantity };
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
+    window.dispatchEvent(new Event('storage'));
+
+    // const product = {
+    //   id: uuidv4,
+    //   name: 'Adara Bed',
+    //   variants: [
+    //     ref{
+    //       id: uuidv4,
+    //       color: ref{
+    //         id: uuidv4,
+    //         name: 'Navy Velvet',
+    //         imageUrl:
+    //       },
+    //       size: ref{
+    //         id: uuidv4,
+    //         name: 'King',
+    //       },
+    //       availableColors: [
+    //         refToColorObj,
+    //       ],
+    //       availableSizes: [
+    //         refToSizeObj
+    //       ]
+    //     }
+    //   ]
+    // }
+  }
 
   return (
     <div className="px-3 md:px-12">
@@ -248,14 +289,26 @@ function Product() {
               />
               <div>
                 <div className="flex items-end gap-1 max-w-xs">
-                  <SpinButton
-                    labelText="Quantity"
-                    inputId="product-quantity-selector"
-                    inputValue={chosenQuantity}
-                    onValueChange={handleQuantityChange}
-                    className="w-20"
-                  />
-                  <PrimaryButton>ADD TO CART</PrimaryButton>
+                  <div className="flex flex-col items-start ">
+                    <label
+                      htmlFor="product-quantity-selector"
+                      className="block font-dm-sans mb-1"
+                    >
+                      Quantity
+                    </label>
+                    <SpinButton
+                      labelText="Quantity"
+                      inputId="product-quantity-selector"
+                      inputValue={chosenQuantity}
+                      onValueChange={handleQuantityChange}
+                      className="min-w-[7rem] md:min-w-[5rem]"
+                    />
+                  </div>
+                  <PrimaryButton
+                    onClick={() => addToCart(product.id, chosenQuantity)}
+                  >
+                    ADD TO CART
+                  </PrimaryButton>
                 </div>
               </div>
               <div className="flex items-center gap-2">
