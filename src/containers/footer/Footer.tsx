8f2a1e4ec6e665ec33b12f8049710477a2d09994
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-06-09 21:15:57 +0200

    Add semantic markup to Footer container

--- src/containers/footer/Footer.tsx
+++ src/containers/footer/Footer.tsx
@@ -68,100 +68,107 @@ function Footer() {
   }
 
   return mediumMatches ? (
-    <div
-      className="mt-auto bg-primary-300 text-text flex gap-5
-            justify-evenly items-start px-5 py-5 font-dm-sans text-lg"
-    >
-      {categories.map((category, index) => {
-        return (
-          <nav className="flex flex-col" key={index}>
-            <h3 className="font-bold text-xl mb-1">{category.header}</h3>
-            <ul>
-              {category.items.map((item, i) => {
-                if (category.header === 'Customer Support') {
-                  return <li key={i}>{item}</li>;
-                }
-                return (
-                  <li key={i}>
-                    <a href="#" className="hover:underline underline-offset-2">
-                      {item}
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
-        );
-      })}
-    </div>
-  ) : (
-    <div
-      className="mt-auto bg-primary-300 text-text flex flex-col
-        justify-start items-stretch font-dm-sans text-lg"
-    >
-      {categories.map((category, index) => {
-        return (
-          <nav className="flex flex-col" key={index}>
-            <div
-              className="flex justify-between items-center border-b-[1px]
-                       border-b-primary-700 px-4 py-2"
-              onClick={() => handleToggleOpenStatus(index)}
-            >
-              <h3 className="font-bold text-lg md:text-xl mb-1">
-                {category.header}
-              </h3>
-              <button
-                type="button"
-                aria-label={
-                  openStatusList[index].isOpen ? 'hide links' : 'show links'
-                }
-                onClick={() => handleToggleOpenStatus(index)}
-              >
-                {openStatusList[index].isOpen ? (
-                  <IconContext.Provider
-                    value={{
-                      style: { strokeWidth: '0.05rem' },
-                      className: 'text-text',
-                      size: '1.5rem',
-                    }}
-                  >
-                    <BiMinus />
-                  </IconContext.Provider>
-                ) : (
-                  <IconContext.Provider
-                    value={{
-                      style: { strokeWidth: '0.05rem' },
-                      className: 'text-text',
-                      size: '1.5rem',
-                    }}
-                  >
-                    <BiPlus />
-                  </IconContext.Provider>
-                )}
-              </button>
-            </div>
-            <div
-              className={`${openStatusList[index].isOpen ? '' : 'hidden'}
-                  px-4 py-3 border-b-[1px] border-b-primary-700
-                  bg-primary-400`}
-            >
-              <ul className="flex flex-col gap-1">
+    <footer className="mt-auto">
+      <div
+        className="bg-primary-300 text-text flex gap-5
+              justify-evenly items-start px-5 py-5 font-dm-sans text-lg"
+      >
+        {categories.map((category, index) => {
+          return (
+            <nav className="flex flex-col" key={index}>
+              <h3 className="font-bold text-xl mb-1">{category.header}</h3>
+              <ul>
                 {category.items.map((item, i) => {
                   if (category.header === 'Customer Support') {
                     return <li key={i}>{item}</li>;
                   }
                   return (
                     <li key={i}>
-                      <a href="#">{item}</a>
+                      <a
+                        href="#"
+                        className="hover:underline underline-offset-2"
+                      >
+                        {item}
+                      </a>
                     </li>
                   );
                 })}
               </ul>
-            </div>
-          </nav>
-        );
-      })}
-    </div>
+            </nav>
+          );
+        })}
+      </div>
+    </footer>
+  ) : (
+    <footer className="mt-auto">
+      <div
+        className="bg-primary-300 text-text flex flex-col
+          justify-start items-stretch font-dm-sans text-lg"
+      >
+        {categories.map((category, index) => {
+          return (
+            <nav className="flex flex-col" key={index}>
+              <div
+                className="flex justify-between items-center border-b-[1px]
+                         border-b-primary-700 px-4 py-2"
+                onClick={() => handleToggleOpenStatus(index)}
+              >
+                <h3 className="font-bold text-lg md:text-xl mb-1">
+                  {category.header}
+                </h3>
+                <button
+                  type="button"
+                  aria-label={
+                    openStatusList[index].isOpen ? 'hide links' : 'show links'
+                  }
+                  onClick={() => handleToggleOpenStatus(index)}
+                >
+                  {openStatusList[index].isOpen ? (
+                    <IconContext.Provider
+                      value={{
+                        style: { strokeWidth: '0.05rem' },
+                        className: 'text-text',
+                        size: '1.5rem',
+                      }}
+                    >
+                      <BiMinus />
+                    </IconContext.Provider>
+                  ) : (
+                    <IconContext.Provider
+                      value={{
+                        style: { strokeWidth: '0.05rem' },
+                        className: 'text-text',
+                        size: '1.5rem',
+                      }}
+                    >
+                      <BiPlus />
+                    </IconContext.Provider>
+                  )}
+                </button>
+              </div>
+              <div
+                className={`${openStatusList[index].isOpen ? '' : 'hidden'}
+                    px-4 py-3 border-b-[1px] border-b-primary-700
+                    bg-primary-400`}
+              >
+                <ul className="flex flex-col gap-1">
+                  {category.items.map((item, i) => {
+                    if (category.header === 'Customer Support') {
+                      return <li key={i}>{item}</li>;
+                    }
+                    return (
+                      <li key={i}>
+                        <a href="#">{item}</a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </nav>
+          );
+        })}
+      </div>
+    </footer>
   );
 }
 export default Footer;
