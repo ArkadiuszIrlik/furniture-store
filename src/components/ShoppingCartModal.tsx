e94bb64491f693040b1f715e22c4bd2297d93338
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-06-11 23:22:15 +0200

    Refactor ShoppingCartModal

--- src/components/ShoppingCartModal.tsx
+++ src/components/ShoppingCartModal.tsx
@@ -1,11 +1,18 @@
-import { forwardRef } from 'react';
-import { BiTrashAlt } from 'react-icons/bi';
-import { IconContext } from 'react-icons';
-import { bed1 } from '../assets';
-import { SpinButton, PrimaryButton } from '.';
+import { forwardRef, Dispatch } from 'react';
+import { PrimaryButton, ShoppingCartList } from '.';
+import { calculateCartTotal } from '../helpers';
+
+interface CartItem {
+  id: string;
+  name: string;
+  details?: string;
+  image: string;
+  priceUsd: number;
+  quantity: number;
+}
 
 const ShoppingCartModal = forwardRef(function ShoppingCartModal(
-  { cart, cartDispatch },
+  { cart, cartDispatch }: { cart: CartItem[]; cartDispatch: Dispatch<any> },
   ref
 ) {
   return (
@@ -24,68 +31,32 @@ const ShoppingCartModal = forwardRef(function ShoppingCartModal(
         SHOPPING CART
       </h3>
       <div
-        className="flex flex-col gap-5 py-4  overflow-y-auto scrollbar
+        className="py-4 overflow-y-auto scrollbar
             scrollbar-thumb-primary-700 scrollbar-track-primary-300
            scrollbar-thumb-rounded-lg pr-2"
         style={{ '--scrollbar-width': '8px' }}
       >
         {cart.length !== 0 ? (
-          cart.map((item, index) => {
-            return (
-              <div className="flex items-center gap-4 font-dm-sans" key={index}>
-                <img
-                  src={bed1}
-                  alt=""
-                  className="rounded-lg flex-auto basis-1/3 min-w-0
-                     object-contain object-center h-36 bg-product"
-                  draggable="false"
-                />
-                <div className="flex flex-col flex-auto min-w-0 basis-2/3 text-left">
-                  <div className="flex justify-between">
-                    <p className="font-medium break-words min-w-0">
-                      Pillow Stuff
-                    </p>
-                    <IconContext.Provider
-                      value={{
-                        size: '1.3rem',
-                        className: 'text-text shrink-0',
-                      }}
-                    >
-                      <BiTrashAlt />
-                    </IconContext.Provider>
-                  </div>
-                  <p className="mb-2">Emerald Velvet / Cal King</p>
-                  <div className="flex justify-stretch items-center gap-3 mb-2">
-                    <label htmlFor="product-quantity-selector-cart-modal">
-                      Quantity
-                    </label>
-                    <SpinButton
-                      labelText="Quantity"
-                      inputId="product-quantity-selector-cart-modal"
-                      inputValue={item.quantity}
-                      onValueChange={() => {}}
-                      className="min-w-[7rem] md:min-w-[5rem]"
-                    />
-                  </div>
-                  <p className="font-medium">$1,249</p>
-                </div>
-              </div>
-            );
-          })
+          <ShoppingCartList cart={cart} cartDispatch={cartDispatch} />
         ) : (
           <p className="font-open-sans text-center">
             There are no items in your cart.
           </p>
         )}
       </div>
-      {cart && Object.keys(cart).length !== 0 && (
+      {cart.length !== 0 && (
         <div
           className="pt-2 pb-4 border-t-[1px]
            border-t-primary-300"
         >
           <div className="flex justify-center gap-36 font-dm-sans font-medium mb-3">
             <p>SUBTOTAL:</p>
-            <p>$1,249</p>
+            <p>
+              $
+              {calculateCartTotal(cart)
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            </p>
           </div>
           <div className="flex flex-col items-center">
             <div className="flex flex-col gap-2 min-w-[10rem]">
