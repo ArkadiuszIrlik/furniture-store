c909f4d887d4e77769e06da552300f30fb603372
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-06-18 13:03:09 +0200

    Add address selection to CheckoutInformation

--- src/containers/checkoutInformation/CheckoutInformation.tsx
+++ src/containers/checkoutInformation/CheckoutInformation.tsx
@@ -1,8 +1,43 @@
 import { BiChevronRight, BiPlus } from 'react-icons/bi';
 import { IconContext } from 'react-icons';
 import { BsFillHouseDoorFill } from 'react-icons/bs';
+import { useEffect, useState } from 'react';
 
 function CheckoutInformation() {
+  const [savedAddressList, setSavedAddressList] = useState<object[]>([]);
+  useEffect(() => {
+    setSavedAddressList([
+      {
+        name: 'Billy Jean',
+        addressLine1: '3687 Glen Street',
+        addressLine2: 'Portland, OR 97205',
+        phoneNumber: '+1 410-414-8519',
+      },
+      {
+        name: 'Billy Jean',
+        addressLine1: '3687 Glen Street',
+        addressLine2: 'Portland, OR 97205',
+        phoneNumber: '+1 410-414-8519',
+      },
+      {
+        name: 'Billy Jean',
+        addressLine1: '3687 Glen Street',
+        addressLine2: 'Portland, OR 97205',
+        phoneNumber: '+1 410-414-8519',
+      },
+    ]);
+  }, []);
+
+  const [selectedAddressIndex, setSelectedAddressIndex] = useState(0);
+
+  function handleChangeAddressIndex(nextIndex: number) {
+    setSelectedAddressIndex(nextIndex);
+  }
+
+  function handleAddAddress() {
+    setSavedAddressList([...savedAddressList, { name: 'New Address' }]);
+  }
+
   return (
     <div className="font-dm-sans px-3 md:px-12 py-4">
       <h1 className="text-3xl sm:mb-1">CHECKOUT</h1>
@@ -42,49 +77,79 @@ function CheckoutInformation() {
       </nav>
       <h2 className="text-2xl mb-2">SHIPPING ADDRESS</h2>
       <div className="flex gap-3">
-        <div className="flex gap-5 rounded-xl border-2 border-primary-700 px-5 py-2">
-          <div className="flex justify-center items-center">
-            <div className="w-5 aspect-square rounded-full border-2 border-primary-700 bg-primary-700" />
-          </div>
-          <div className="flex flex-col">
-            <p>John Doe</p>
-            <p>3012 Brooklyn Street</p>
-            <p>Portland, OR 97205</p>
-            <p>+1 410-414-8519</p>
-          </div>
-        </div>
-        <div className="flex gap-5 rounded-xl border-2 border-zinc-400 px-5 py-2">
-          <div className="flex justify-center items-center">
-            <div className="w-5 aspect-square rounded-full border-2 border-zinc-400" />
-          </div>
-          <div className="flex flex-col">
-            <p>John Doe</p>
-            <p>3012 Brooklyn Street</p>
-            <p>Portland, OR 97205</p>
-            <p>+1 410-414-8519</p>
-          </div>
-        </div>
-        <div className="flex flex-col justify-center gap-2 rounded-xl border-2 border-zinc-400 px-5 py-2">
-          <div className="flex justify-center items-end">
-            <IconContext.Provider
-              value={{
-                size: '1.7rem',
-                className: 'text-zinc-400 -mr-1',
-              }}
+        <fieldset className="flex flex-wrap gap-3">
+          <legend className="sr-only">Choose shipping address</legend>
+          {savedAddressList.map((address, index) => (
+            <label
+              htmlFor={`saved-address-${index}`}
+              className={`flex gap-5 rounded-xl border-2 ${
+                selectedAddressIndex === index
+                  ? 'border-primary-700'
+                  : 'border-zinc-400 hover:bg-zinc-100/75 hover:border-primary-700'
+              } px-5 py-2 cursor-pointer group`}
+              aria-label={Object.values(address).join(', ')}
+              key={index}
             >
-              <BiPlus />
-            </IconContext.Provider>
-            <IconContext.Provider
-              value={{
-                size: '2.5rem',
-                className: 'text-zinc-400 ',
-              }}
-            >
-              <BsFillHouseDoorFill />
-            </IconContext.Provider>
-          </div>
-          <p>Add new address</p>
-        </div>
+              <div className="flex justify-center items-center">
+                <div
+                  className={`w-5 aspect-square rounded-full border-2 ${
+                    selectedAddressIndex === index
+                      ? 'border-primary-700 bg-primary-700'
+                      : 'border-zinc-400 group-hover:border-primary-700'
+                  }`}
+                />
+              </div>
+              <div
+                className="flex flex-col justify-center"
+                aria-hidden="true"
+                id=""
+              >
+                {Object.values(address).map((addressLine, lineIndex) => (
+                  <p key={lineIndex}>{addressLine}</p>
+                ))}
+              </div>
+              <input
+                type="radio"
+                name="saved-address"
+                id={`saved-address-${index}`}
+                value={index}
+                checked={index === selectedAddressIndex}
+                onChange={(e) =>
+                  handleChangeAddressIndex(Number(e.target.value))
+                }
+                className="sr-only"
+              />
+            </label>
+          ))}
+          <button
+            type="button"
+            className="flex flex-col justify-center items-center gap-2
+             rounded-xl border-2 border-zinc-400 px-5 py-2 hover:bg-zinc-100
+              hover:border-primary-700"
+            onClick={handleAddAddress}
+            aria-label="Add new address"
+          >
+            <div className="flex justify-center items-end">
+              <IconContext.Provider
+                value={{
+                  size: '1.7rem',
+                  className: 'text-zinc-400 -mr-1',
+                }}
+              >
+                <BiPlus />
+              </IconContext.Provider>
+              <IconContext.Provider
+                value={{
+                  size: '2.5rem',
+                  className: 'text-zinc-400 ',
+                }}
+              >
+                <BsFillHouseDoorFill />
+              </IconContext.Provider>
+            </div>
+            <p aria-hidden="true">Add new address</p>
+          </button>
+        </fieldset>
       </div>
     </div>
   );
