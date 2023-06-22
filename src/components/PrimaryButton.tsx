7c267fecdec6cc00eef816ff2aa049f41c1b2fe9
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-06-22 15:59:08 +0200

    Add styling to PrimaryButton in 'disabled' state

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -3,20 +3,31 @@ function PrimaryButton({
   className = '',
   isFilled = 'true',
   onClick = () => undefined,
+  type = 'button',
+  isDisabled = false,
 }: {
   children?: any;
   isFilled?: 'true' | 'false';
   className?: string;
   onClick?: any;
+  type: 'submit' | 'reset' | 'button';
+  isDisabled?: boolean;
 }) {
+  let conditionalClassName;
+  if (isDisabled) {
+    conditionalClassName = 'bg-gray-200 text-gray-500 border-gray-200';
+  } else if (isFilled === 'true') {
+    conditionalClassName = 'bg-primary-700 border-primary-700 text-white';
+  } else {
+    conditionalClassName = 'border-primary-700 text-primary-700';
+  }
   return (
     <button
-      type="button"
-      className={`rounded-lg border-2 border-primary-700 font-dm-sans
-       font-medium px-2 py-2 flex-auto ${
-         isFilled === 'true' ? 'bg-primary-700 text-white' : 'text-primary-700'
-       } ${className}`}
+      type={type}
+      className={`rounded-lg border-2 font-dm-sans
+       font-medium px-2 py-2 flex-auto ${conditionalClassName} ${className}`}
       onClick={onClick}
+      disabled={isDisabled}
     >
       {children}
     </button>
