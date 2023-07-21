310a7f51470fea8de9cad07c24e7d425031e84ac
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-07-21 18:12:34 +0200

    Configure global CSS for Next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,19 @@
+import './global.css';
+
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en">
-        <body className="mx-auto lg:mx-[8%] xl:mx-[14%] 2xl:mx-auto
-     2xl:max-w-[1800px] min-h-screen text-text flex flex-col
-     font-dm-sans">{children}</body>
-      </html>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className="mx-auto flex min-h-screen flex-col
+     font-dm-sans text-text lg:mx-[8%] xl:mx-[14%]
+     2xl:mx-auto 2xl:max-w-[1800px]"
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
