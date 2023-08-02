b19b7c04a7d60cbb3e5834b54d0c5d813a3ca257
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-08-02 20:36:39 +0200

    Fix CartProvider not working with SSR
    CartProvider was causing an error during server prerender, because it
    was accessing localStorage. Added an extra check and useEffect to work
    around that.

--- src/context/CartProvider.tsx
+++ src/context/CartProvider.tsx
@@ -3,19 +3,27 @@
 import { useReducer, useEffect } from 'react';
 import { cartReducer } from 'reducers';
 import { createContext } from 'react';
+import { CartItem, CartAction } from 'reducers/cartReducer';
+
+export const CartContext = createContext<{
+  cart: CartItem[];
+  cartDispatch: React.Dispatch<CartAction>;
+}>({ cart: [], cartDispatch: () => void '' });
+
 function CartProvider({ children }: { children: React.ReactNode }) {
-  const [cart, cartDispatch] = useReducer(
-    cartReducer,
-    localStorage.getItem('cart')
-      ? JSON.parse(localStorage.getItem('cart') ?? '')
-      : []
-  );
-
-  useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(cart));
-  }, [cart]);
-
-  const CartContext = createContext({ cart, cartDispatch });
+  let cart: CartItem[] = [];
+  let cartDispatch: React.Dispatch<CartAction> = () => void '';
+  if (typeof window !== 'undefined') {
+    [cart, cartDispatch] = useReducer(
+      cartReducer,
+      localStorage.getItem('cart')
+        ? JSON.parse(localStorage.getItem('cart') ?? '')
+        : []
+    );
+    useEffect(() => {
+      localStorage.setItem('cart', JSON.stringify(cart));
+    }, [cart]);
+  }
 
   return (
     <CartContext.Provider value={{ cart, cartDispatch }}>
