edd756b2fd79beab403dea36059778f71ecfab54
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-08-03 16:58:38 +0200

    Add components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import {
+  HomeFeaturesSection,
+  HomePhotosSection,
+  InstagramCarousel,
+} from 'components';
+import { Footer } from 'containers';
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
-  }
+  return (
+    <div>
+      <HomeFeaturesSection />
+      <div className="mb-12 px-0 sm:px-10">
+        <HomePhotosSection />
+      </div>
+      <p className="mb-4 text-center text-3xl">
+        GET INSPIRED WITH #ELEVAFAMILY
+      </p>
+      <div className="mb-14">
+        <InstagramCarousel />
+      </div>
+      <Footer />
+    </div>
+  );
+}
