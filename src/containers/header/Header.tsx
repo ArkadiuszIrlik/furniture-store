f10ce8f14c1cfccf37bc47023798c13f746515c6
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-08-03 16:41:00 +0200

    Refactor Header container into a server component

--- src/containers/header/Header.tsx
+++ src/containers/header/Header.tsx
@@ -1,138 +1,61 @@
-import { CiUser } from 'react-icons/ci';
-import { FiShoppingCart } from 'react-icons/fi';
-import { IconContext } from 'react-icons';
-import { HiMenu } from 'react-icons/hi';
-import resolveConfig from 'tailwindcss/resolveConfig';
-import { useState, useRef, Dispatch } from 'react';
-import { ElevaLogo } from '../../assets';
-import tailwindConfig from '../../../tailwind.config';
-import {
-  Search,
-  HeaderNavbar,
-  ShoppingCartModal,
-  CartQuantityCounter,
-} from '../../components';
-import { useMediaQuery } from '../../hooks';
+// import { CiUser } from 'react-icons/ci';
+// import { HiMenu } from 'react-icons/hi';
+import { ElevaLogo } from 'assets';
+import { Search, HeaderNavbar, HeaderCartIcon } from 'components';
+import { textColor } from 'styleVars.js';
+import { IconProvider } from 'context';
+import Image from 'next/image';
+import IconWrapper from 'components/IconWrapper';
+import { CiUser } from 'assets/react-icons';
+import { HiMenu } from 'assets/react-icons';
 
-interface CartItem {
-  id: string;
-  name: string;
-  details?: string;
-  image: string;
-  priceUsd: number;
-  quantity: number;
-}
+// async function exportServerWrapper(component) {
+//   'use server';
+//   return component;
+// }
 
-function Header({
-  cart,
-  cartDispatch,
-}: {
-  cart: CartItem[];
-  cartDispatch: Dispatch<any>;
-}) {
-  const fullConfig = resolveConfig(tailwindConfig);
-  const textColor = fullConfig.theme.colors.text;
-  const mediumScreen = fullConfig.theme.screens.md;
-  const mediumMatches = useMediaQuery(`(min-width: ${mediumScreen})`);
-
-  const [isCartModalShown, setIsCartModalShown] = useState(false);
-  const cartModalRef = useRef<HTMLInputElement | null>(null);
-  const totalQuantity = cart.reduce((total, next) => total + next.quantity, 0);
-
-  function showCartModal() {
-    setIsCartModalShown(true);
-  }
-  async function hideCartModal() {
-    if (!cartModalRef.current) {
-      return;
-    }
-    cartModalRef.current.classList.remove(
-      'animate-[slide-up-fade-in_0.4s_ease-out]'
-    );
-    cartModalRef.current.classList.add(
-      'animate-[slide-down-fade-out_0.4s_ease-out]'
-    );
-    async function animationEnd(): Promise<void> {
-      return new Promise<void>((resolve): void => {
-        if (cartModalRef.current) {
-          cartModalRef.current.onanimationend = () => resolve();
-        }
-      });
-    }
-    await animationEnd();
-    setIsCartModalShown(false);
-  }
-
+function Header() {
   return (
     <header>
       <div
         className="grid grid-cols-[1fr_minmax(max-content,_60%)_1fr]
            grid-rows-[repeat(2,_max-content)] items-center
-            pt-1 md:pt-5 pb-2 px-3 md:px-12 gap-y-2 md:gap-y-4"
+            gap-y-2 px-3 pb-2 pt-1 md:gap-y-4 md:px-12 md:pt-5"
       >
-        <IconContext.Provider
-          value={{
-            style: { strokeWidth: '0.05rem', stroke: textColor },
-            size: '1.5rem',
-          }}
-        >
-          {!mediumMatches && <HiMenu />}
-        </IconContext.Provider>
-        <img
+        <div className="block md:hidden">
+          <IconProvider
+            value={{
+              style: { strokeWidth: '0.05rem', stroke: textColor },
+              size: '1.5rem',
+            }}
+          >
+            <HiMenu />
+          </IconProvider>
+        </div>
+        <Image
           src={ElevaLogo}
           alt="Eleva Design Logo"
-          srcSet=""
-          className="justify-self-center md:justify-self-start w-28"
+          className="w-28 justify-self-center md:justify-self-start"
         />
         <div className="col-span-full row-start-2 md:col-span-1 md:col-start-2 md:row-start-1">
           <Search />
         </div>
-        <div className="flex gap-2 justify-end items-center">
-          <button onClick={() => console.log(`clicked user${Math.random()}`)}>
-            <IconContext.Provider
+        <div className="flex items-center justify-end gap-2">
+          <button>
+            <IconProvider
               value={{
                 style: { strokeWidth: '0.05rem', stroke: textColor },
                 size: '1.5rem',
               }}
             >
               <CiUser />
-            </IconContext.Provider>
+            </IconProvider>
           </button>
-          <div
-            className="relative group flex items-end"
-            onMouseEnter={showCartModal}
-            onMouseLeave={hideCartModal}
-          >
-            <button
-              type="button"
-              onClick={() => console.log(`clicked${Math.random()}`)}
-            >
-              <IconContext.Provider
-                value={{
-                  style: { strokeWidth: '0.1rem', stroke: textColor },
-                  size: '1.5rem',
-                }}
-              >
-                {/* <div className="peer"> */}
-                <FiShoppingCart />
-                {/* </div> */}
-              </IconContext.Provider>
-            </button>
-            <CartQuantityCounter quantity={totalQuantity} />
-            {isCartModalShown && (
-              <ShoppingCartModal
-                ref={cartModalRef}
-                cart={cart}
-                cartDispatch={cartDispatch}
-              />
-            )}
-          </div>
+          <HeaderCartIcon />
         </div>
-        {mediumMatches && (
-          <div className="col-span-full row-start-2 ">
-            <HeaderNavbar />
-          </div>
-        )}
+        <div className="col-span-full row-start-2 hidden md:block">
+          <HeaderNavbar />
+        </div>
       </div>
     </header>
   );
