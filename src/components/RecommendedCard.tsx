30fcd07ce6e70cd0b984a1d8cde1f4f27538f959
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-09-20 19:31:38 +0200

    Refactor RecommendedCard
    Made the component display actual data it receives through props.
    Added typings for the props.
    Used the formatPriceDollars helper to display the price correctly.

--- src/components/RecommendedCard.tsx
+++ src/components/RecommendedCard.tsx
@@ -1,19 +1,23 @@
-import { AdaraBed } from '../assets';
+import Image from 'next/image';
+import { formatPriceDollars } from 'helpers';
 
-function RecommendedCard({ className }) {
+function RecommendedCard({
+  name,
+  image,
+  priceUsd,
+}: {
+  name: string;
+  image: string;
+  priceUsd: number | string;
+}) {
   return (
     <article>
-      <div className={`rounded-lg bg-product p-4 ${className}`}>
-        <div>
-          <img src={AdaraBed} alt="Product" srcSet="" />
-        </div>
-        <div className="mb-2">
-          <p className="font-open-sans font-semibold">Adara Bed</p>
-        </div>
-        <div className="flex">
-          <div className="rounded-2xl bg-primary-300 py-[0.125rem] px-3 ml-auto">
-            <p className="font-open-sans font-semibold">$1,149</p>
-          </div>
+      <div className="flex flex-col rounded-3xl border-primary-300 bg-zinc-100 p-5">
+        {/* remove the width and height from Image */}
+        <Image src={image} width={1000} height={2000} alt="" />
+        <p className="mb-2 font-open-sans font-semibold">{name}</p>
+        <div className="ml-auto rounded-xl bg-primary-300 px-5">
+          <p className="">{formatPriceDollars(priceUsd)}</p>
         </div>
       </div>
     </article>
