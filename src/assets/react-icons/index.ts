71b138e7422f5694c923e938505f6422f8920d97
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-09-20 19:35:46 +0200

    Export new icons from the local react-icons index

--- src/assets/react-icons/index.ts
+++ src/assets/react-icons/index.ts
@@ -3,3 +3,5 @@
 export { CiUser } from 'react-icons/ci';
 export { HiMenu } from 'react-icons/hi';
 export { BiPlus, BiMinus } from 'react-icons/bi';
+export { IoClose } from 'react-icons/io5';
+export { BsPlusLg } from 'react-icons/bs';
