7720ecc784e64b6a1b3defd4c64ba48565f6a6a4
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-09-21 18:48:10 +0200

    Clean up PathDisplay
    Reordered tailwind classes and added a missing "key" attribute.

--- src/components/PathDisplay.tsx
+++ src/components/PathDisplay.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from 'react';
+
 export interface Path {
   name: string;
   id: string;
@@ -6,19 +8,19 @@ export interface Path {
 
 function PathDisplay({ pathArray }: { pathArray: Path[] }) {
   return (
-    <p className="font-dm-sans font-bold text-base text-primary-700">
+    <p className="font-dm-sans text-base font-bold text-primary-700">
       <a href="#" className="hover:text-accents-700">
         Home
       </a>{' '}
       /{' '}
       {pathArray.map((destination, i, arr) => {
         return (
-          <>
+          <Fragment key={i}>
             <a href="#" className="hover:text-accents-700">
               {destination.name}
             </a>
             {i < arr.length - 1 ? ' / ' : undefined}
-          </>
+          </Fragment>
         );
       })}
     </p>
