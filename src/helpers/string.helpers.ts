a1619a5e9e4665c0970ad725662fd978b117e76c
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-09-26 19:55:06 +0200

    Add capitalizeEachWord helper function
    Added a helper function to capitalize the first letter in each word of
    a provided string.
    
    Also created a string.helpers.ts file to organize all the future helper
    functions related to manipulating strings.

---
+++ src/helpers/string.helpers.ts
@@ -0,0 +1,8 @@
+export function capitalizeEachWord(string: string) {
+  return string
+    .split(' ')
+    .map((word) => {
+      return word[0].toUpperCase() + word.slice(1);
+    })
+    .join(' ');
+}
