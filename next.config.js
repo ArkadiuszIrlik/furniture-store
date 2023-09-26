246d731e9980be555cc8e0cda1bc17c29ec7d7d6
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-09-26 19:45:30 +0200

    Suppress mongo driver dependency errors
    Mongoose doesn't install the optional mongodb node driver dependencies,
    causing webpack to throw errors every time one of the optional
    dependencies is called with 'require()'.
    
    As per this developer post:
    'https://github.com/Automattic/mongoose/issues/8602#issuecomment-596856448'
    there is no way to actually fix this issue, other than manually
    installing all those unnecessary dependencies.
    
    With that in consideration, I chose to suppress errors related to those
    dependencies inside of webpack. This commit adds them to webpack ignore
    list inside the next.config file.

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config, options) {
+    const pluginsToAdd = [
+      new options.webpack.IgnorePlugin({
+        resourceRegExp:
+          /(@aws-sdk\/credential-providers)|(kerberos)|(@mongodb-js\/zstd)|(snappy)|(aws4)|(mongodb-client-encryption)/,
+        contextRegExp: /mongodb\/lib$/,
+      }),
+    ];
+    if (config.plugins) {
+      config.plugins.push(...pluginsToAdd);
+    } else {
+      config.plugins = pluginsToAdd;
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
