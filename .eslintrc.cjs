d22f31bb28c6d07925fc78ecd88bf4fba995573b
Arkadiusz Irlik <arkadiuszirlik@outlook.com>
2023-10-30 18:27:31 +0100

    Update eslint rules
    Removed the rule that forbids prefixing variables with underscores.
    
    Mongo stores its ID's in a property called _id, so eslint was showing
    an error every time the property was accessed. It could technically be
    worked around by changing the return documents in an aggregation stage
    during database fetches, but I think it's more convenient to just
    disable the rule considering it's not particularly helpful regardless.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,5 +22,6 @@ module.exports = {
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 0,
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
   },
 };
